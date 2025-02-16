7dc4953b3b79630d5a1186aa7e5354c7a30cc663
nikee-l <gritveronika@gmail.com>
2025-02-16 15:33:05 +0200

    Add files via upload

--- maze/main.py
+++ maze/main.py
@@ -1,13 +1,30 @@
-from pygame import*
+from pygame import *
 
-display.set_caption('Моя первая игра')
+class GameSprite(sprite.Sprite):
+    def __init__(self,picture,w,h,x,y): 
+        super().__init__()
+        self.image=transform.scale(image.load(picture),(w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+        
+
+
+
 window_width = 700
 window_height = 500
+display.set_caption("Лабіринт")
 window = display.set_mode((window_width, window_height))
 
-run = True 
-while run: 
+run = True
+wall_1 = GameSprite('assets/wall.png',80,180,200,250)
+
+while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+    window.fill((150, 240, 200))
+    wall_1.reset()
     display.update()
