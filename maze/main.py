de8ca61fef34a98c9686807c978fee78d1365a22
nikee-l <gritveronika@gmail.com>
2025-02-23 15:30:53 +0200

    Add files via upload

--- maze/main.py
+++ maze/main.py
@@ -10,6 +10,21 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
         
+    def __init__(self, player_image, player_x, player_y, size_x, size_y):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_x_speed, player_y_speed):
+        GameSprite.__init__(self, player_image, player_x, player_y, size_x, size_y)
+        self.x_speed = player_x_speed
+        self.y_speed = player_y_speed
+
+
 
 
 
@@ -19,7 +34,7 @@ display.set_caption("Лабіринт")
 window = display.set_mode((window_width, window_height))
 
 run = True
-wall_1 = GameSprite('assets/wall.png',80,180,200,250)
+wall_1 = GameSprite('assets\\wall.png',80,180,200,250)
 
 while run:
     for e in event.get():
